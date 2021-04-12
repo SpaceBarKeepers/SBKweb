ac845b2ee66dcdf3750bb1daea747cc4ae225191
Bara Maresova <bbmaresova@gmail.com>
2021-04-12 20:59:10 +0200

    draft mobilni verze

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contacts.scss';
+
+export const Contacts = () => {
+  return (
+    <div className="contacts_div">
+      <h2>Kontakty</h2>
+      <div className="table"></div>
+      <div className="contactsDetails">Telefon, email, adresa</div>
+    </div>
+  );
+};
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.scss';
+
+export const Main = () => {
+  return (
+    <div className="main_div">
+      <div className="buttons">
+        <div className="buttons_products">Produkty</div>
+        <div className="buttons_about">O nás</div>
+      </div>
+      <div className="us_img"></div>
+    </div>
+  );
+};
