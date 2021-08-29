82573307538fdfd45d56be8ef185a7c6eeb2f5e8
Bara Maresova <bbmaresova@gmail.com>
2021-08-29 20:19:36 +0200

    zoom window a raketa

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,62 @@
 import React from 'react';
 import window from '../../images/graphics/Window.png';
 import { motion } from 'framer-motion';
+import Navbar from '../../components/Navbar/Navbar';
 
+import textJSON from '../../texts.json';
 import './about.scss';
 
-const aboutVariants = {
-  hidden: { z: -1 },
-  visible: { z: 0, transition: { ease: 'easeIn', duration: 4 } },
-};
+const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
 
-export const About = () => {
+export const About = ({ imageDetails }) => {
   return (
-    <motion.section
-      className="about_page"
-      style={{ backgroundImage: `url(${window})` }}
-      variants={aboutVariants}
-      initial="hidden"
-      animate="visible"
-    >
-      O nás - info
-    </motion.section>
+    <section className="about_page">
+      <Navbar />
+      <motion.div className="image-container-single">
+        <motion.div
+          initial={{
+            y: 0,
+            width: imageDetails.width,
+            height: imageDetails.height,
+          }}
+          animate={{
+            y: '-40%',
+            width: '110%',
+            height: window.innerWidth > 1440 ? 1100 : 400,
+            transition: { delay: 0.2, ...transition },
+          }}
+          className="thumbnail-single"
+        >
+          <motion.div
+            className="frame-single"
+            whileHover="hover"
+            transition={transition}
+          >
+            <motion.img
+              src={window}
+              alt="window"
+              initial={{ y: '10%', scale: 1.0 }}
+              animate={{
+                transition: { delay: 0.2, ...transition },
+                y: 0,
+              }}
+            />
+            <motion.div
+              initial={{ z: 0, y: 0, opacity: 0 }}
+              animate={{ z: 0, y: '40%', opacity: 1 }}
+              transition={transition}
+              className="aboutUs_content"
+            >
+              <h2>O nás</h2>
+              <p>{textJSON.aboutUs.mainText}</p>
+              <h3>Bára</h3>
+              <p>{textJSON.aboutUs.bara}</p>
+              <h3>Kája</h3>
+              <p>{textJSON.aboutUs.karel}</p>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>{' '}
+    </section>
   );
 };
---
+++ src/pages/InitialDesktop/InitialDesktop.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import rocket from '../../images/graphics/RaketaNosign.png';
+import './initialDesktop.scss';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
+
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+/* const mainVariants = {
+  animate: {
+    transition: {
+      staggerChildren: 1.04,
+    },
+  },
+}; */
+const rocketVariants = {
+  initial: { y: 0 },
+  animate: {
+    y: '60%',
+    scale: 7,
+    transition: {
+      delay: 0.3,
+      duration: 2.5,
+      ease: [0.33, 0.43, 0.53, 0.83],
+    },
+  },
+};
+
+function InitialDesktop() {
+  return (
+    <motion.main animate="animate" className="initialDesktop">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: {
+            delay: 2,
+            duration: 0.4,
+          },
+        }}
+      >
+        <Navbar />{' '}
+      </motion.div>
+      <div className="iDthumbnail">
+        <motion.div
+          className="iDframe"
+          variants={rocketVariants}
+          animate="animate"
+          initial="initial"
+        >
+            <motion.img src={rocket} alt="okno" transition={transition} />
+        </motion.div>
+      </div>
+    </motion.main>
+  );
+}
+
+export default InitialDesktop;
