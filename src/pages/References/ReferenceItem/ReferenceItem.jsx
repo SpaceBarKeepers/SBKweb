e7025303109841d46905f26083f150a1bd9ee1b2
Karel Dohnal <karel@motv.eu>
2021-09-18 10:39:03 +0200

    Reference animation restart

--- src/pages/References/ReferenceItem/ReferenceItem.jsx
+++ src/pages/References/ReferenceItem/ReferenceItem.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, {useEffect} from 'react';
+import {motion, useAnimation} from 'framer-motion';
 import './referenceItem.scss';
+import {useInView} from "react-intersection-observer";
 
 function ReferenceItem(props) {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+    if (!inView) {
+      controls.start('hidden');
+    }
+  }, [controls, inView]);
+
+  const referenceItemVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      backgroundColor: 'rgba(227, 11, 93, 0.9)',
+      transition: { duration: 1.1, delay: 1 },
+    },
+  };
+
   return (
     <div
       className="referenceItem"
       style={{ backgroundImage: `url(${props.refImg})` }}
     >
       <motion.div
+        ref={ref}
         className="referenceContent"
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          backgroundColor: 'rgba(227, 11, 93, 0.9)',
-          transition: { duration: 1.1, delay: 1 },
-        }}
+        initial="hidden"
+        animate={controls}
+        variants={referenceItemVariants}
       >
         <h3>{props.refName}</h3>
         <p>{props.refDetails}</p>
