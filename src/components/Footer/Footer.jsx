00b68ae514bc5d1d695bf30975d944a8e434d18a
Bara Maresova <bbmaresova@gmail.com>
2021-10-23 13:32:10 +0200

    cookies consent 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,12 +6,15 @@ import Logo from '../Logo/Logo';
 function Footer() {
   return (
     <footer className="footer">
-      <Logo/>
+      <Logo />
       <div className="footer_contact">
         <a href="mailto:info@spacebarkeepers.com">info@spacebarkeepers.com</a>
         <p>+420&nbsp;601&nbsp;563&nbsp;231</p>
       </div>
       <FooterSome />
+  {/*     <div id="changePreferencesWrapper">
+        <button id="changePreferences">Změnit nastavení cookies</button>
+      </div> */}
     </footer>
   );
 }
