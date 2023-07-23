3447bd6d1cbd301dd641e6781c42d14f4ae5324f
Karel Dohnal <karel.dohnal@gmail.com>
2023-07-23 19:54:49 +0200

    NavItem fix (hover, click on li)

--- src/components/Header/NavItem.jsx
+++ src/components/Header/NavItem.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './navItem.scss';
 
 function NavItem({ linkHash, navItemName }) {
+
+  const handleClick = (hash) => () => {
+    window.location.hash = hash;
+  }
+
   return (
-    <li className="navItem">
-      <a href={linkHash}>{navItemName}</a>
+    <li className="navItem" onClick={handleClick(linkHash)}>
+      <button onClick={handleClick(linkHash)}>{navItemName}</button>
     </li>
   );
 }
